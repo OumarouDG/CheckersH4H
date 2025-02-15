ffc3ed8642bcbb36a2747717f43f5a5d8fa3eeff
Emily Ramos <@eramos@scu.edu>
2025-02-15 14:47:16 -0800

    installed sass
    we can now use scss with our code

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,10 @@
 import './App.css';
-import Navbar from 'frontend/src/component/Navbar/Navbar.jsx';
+import Navbar from "./component/Navbar/Navbar";
 
 function App() {
   return (
     <div className="App">
+      <Navbar></Navbar>
       <header className="App-header">
         <h1>Welcome to Your React Chrome Extension!</h1>
         <p>This is a simple Chrome extension built with React.</p>

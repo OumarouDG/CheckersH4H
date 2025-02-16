1bec88fc7ca2dcd71fe6786a933127b64a41a445
JohnJamison <www.johnjamison@gmail.com>
2025-02-15 23:30:52 -0800

    Search Algo pt2

--- backend/searchalgo/urls.py
+++ backend/searchalgo/urls.py
@@ -14,12 +14,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('myapp.urls')),  # Include the app's URLs
-    path('deepseek/', include('deepseek.urls')),
-    path('searchalgo/', include('searchalgo.urls'))
+    path('url-name', 'function-name'.views.search_view, name='new name'),
 ]

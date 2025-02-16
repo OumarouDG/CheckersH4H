f2f6073189a60ed09eacc8afd01cb16279893786
Benson Tran <lolbensontheburrito@gmail.com>
2025-02-15 16:56:51 -0800

    pushing nav bar update
    asdad

--- frontend/src/component/Navbar/Navbar.jsx
+++ frontend/src/component/Navbar/Navbar.jsx
@@ -1,28 +1,55 @@
 import React from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faHouse, faUser, faGear } from "@fortawesome/free-solid-svg-icons";
-import "./Navbar.scss"; // Import SCSS file for styling
+import { Link } from "react-router-dom"; // Import for routing
+import { AppBar, Toolbar, IconButton, Typography } from "@mui/material";
+import { Home, Person, Settings, Article } from "@mui/icons-material"; // Import Article icon
 
 const Navbar = () => {
   return (
-    <nav className="navbar">
-      <ul className="nav-list">
-        <li className="nav-item">
-          <FontAwesomeIcon icon={faHouse} />
-          <span>Home</span>
-        </li>
-        <li className="nav-item">
-          <FontAwesomeIcon icon={faUser} />
-          <span>Profile</span>
-        </li>
-        <li className="nav-item">
-          <FontAwesomeIcon icon={faGear} />
-          <span>Settings</span>
-        </li>
-      </ul>
-    </nav>
+    <AppBar
+      position="static"
+      sx={{
+        backgroundColor: "#333", // Dark gray background
+        borderRadius: "12px",
+        width: "90%",
+        margin: "10px auto",
+        padding: "8px 0",
+        color: "white", // Set text/icons to white
+        boxShadow: "0px 4px 10px rgba(0, 0, 0, 0.1)", // Light shadow
+        zIndex: 2, // Ensure navbar stays on top
+        position: "relative", // Required for z-index to take effect
+      }}
+    >
+      <Toolbar sx={{ display: "flex", justifyContent: "space-around" }}>
+        <IconButton sx={{ color: "white" }}>
+          <Home fontSize="small" />
+          <Typography variant="body2" sx={{ marginLeft: 0.5, color: "white" }}>
+            Home
+          </Typography>
+        </IconButton>
+        <IconButton sx={{ color: "white" }}>
+          <Person fontSize="small" />
+          <Typography variant="body2" sx={{ marginLeft: 0.5, color: "white" }}>
+            Profile
+          </Typography>
+        </IconButton>
+        <IconButton sx={{ color: "white" }}>
+          <Settings fontSize="small" />
+          <Typography variant="body2" sx={{ marginLeft: 0.5, color: "white" }}>
+            Settings
+          </Typography>
+        </IconButton>
+        {/* New Modern News Icon with Routing */}
+        <Link to="/news" style={{ textDecoration: "none", color: "white" }}>
+          <IconButton sx={{ color: "white" }}>
+            <Article fontSize="small" />
+            <Typography variant="body2" sx={{ marginLeft: 0.5, color: "white" }}>
+              Modern News
+            </Typography>
+          </IconButton>
+        </Link>
+      </Toolbar>
+    </AppBar>
   );
 };
 
 export default Navbar;
-
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,17 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// index.js
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import React from "react";
+import ReactDOM from "react-dom/client"; // Use the client version of react-dom
+import { BrowserRouter } from "react-router-dom"; // Import BrowserRouter
+import App from "./App"; // Your main App component
+
+const root = ReactDOM.createRoot(document.getElementById("root")); // Create root element
 root.render(
-  <React.StrictMode>
+  <BrowserRouter>  {/* Wrap your app with BrowserRouter */}
     <App />
-  </React.StrictMode>
+  </BrowserRouter>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
